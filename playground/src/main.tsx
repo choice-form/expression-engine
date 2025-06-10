f2f01371d36ed95b213cc9c7f2b6d1133cb5a04b
Wester <wester@choiceform.com>
2025-06-10 14:29:07 +0800

    fix: add basename to React Router for GitHub Pages deployment

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -5,7 +5,7 @@ import App from "./app"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/expression-engine">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
